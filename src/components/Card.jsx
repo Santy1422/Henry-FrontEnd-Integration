7c8b8a7ef64c2595dfd61cc76516127b16a3be74
Santy1422 <102694038+Santy1422@users.noreply.github.com>
2022-11-13 20:40:35 +0100

    Add files via upload

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,29 +1,107 @@
 import styled from "styled-components"
 import { Link } from "react-router-dom"
+import * as actions from "../redux/actions"
+import { useDispatch } from "react-redux"
+import { useState } from "react"
+import { useSelector } from "react-redux"
+import { useEffect } from "react"
+
 const DivCard = styled.div`
-background-color: #9b9badcf;
+margin-top: 3%;
+background-color: #c2c2cccf;
+border-radius: 10px;
+border: solid 2px;
+&:hover{
+   background-color: #9b9badcf;
+border-radius: 10px;
+border: solid 2px;
+}
+
 `
 const Botonse = styled.button`
 color:white;
 background-color: red;
 border-radius: 10%;
+&:hover{
+color:white;
+background-color: #b50404;
+border-radius: 10%;
+}
+`
+const Imgcard = styled.img`
+display: flex;
+justify-content: center;
+border-radius: 30%;
+border: solid 1px;
+
+&:hover{
+   display: flex;
+justify-content: center;
+border-radius: 30%;
+border: solid 1px;
+   -webkit-transform: scale(1.1);
+    -moz-transform: scale(1.1);
+    -o-transform: scale(1.1);
+    -ms-transform: scale(1.1);
+    transform: scale(1.1)
+}
+`
+const H2styled = styled.h3`
+display: flex;
+justify-content: center;
+`
+const Blikes = styled.button`
+&:hover{
+
+-webkit-transform: scale(1.1);
+    -moz-transform: scale(1.1);
+    -o-transform: scale(1.1);
+    -ms-transform: scale(1.1);
+    transform: scale(1.1)
+}
 `
 
-
 export default function Card(props) {
+const estadoglobal = useSelector(state => state.myFavorites)
+const dispatch = useDispatch()
 const aux = () =>{
 props.onClose(props.name)
 
 }
-   console.log(props)
+const [isFav, setIsFav] = useState(false)
+
+const handleFavorite = () =>{
+if(isFav === true){
+
+   setIsFav(false)   
+   dispatch(actions.deletes(props.id))
+} else {
+   setIsFav(true)   
+   dispatch(actions.agregar(props))
+}
+}
+
+useEffect(() => {
+   estadoglobal.forEach((fav) => {
+      if (fav.id === props.id) {
+         setIsFav(true);
+      }
+   });
+}, [props.estadoglobal]);
+
    return (
-      <DivCard>
-      <Botonse onClick={() => props.onClose(props.detailIds)}>X</Botonse>
-      
-      <Link to={`/detail/${props.detailId}`} >  <h2 key={props.key}>{props.name}</h2></Link>
-      <h2>{props.species}</h2>
-      <h2>{props.gender}</h2>
-      <img src={props.image} alt="" />
+      <DivCard className="asdf">
+      <Botonse onClick={aux}>X</Botonse>
+      <Link to={`/detail/${props.detailId}`} >  <H2styled key={props.key}>{props.name}</H2styled></Link>
+      <H2styled>{props.species}</H2styled>
+      <H2styled>{props.gender}</H2styled>
+      <Imgcard src={props.image} alt="" />
+      {isFav ? (
+					<button onClick={handleFavorite}>❤️</button>
+				) : (
+					<button onClick={handleFavorite}>🤍</button>
+				)}
+
    </DivCard>
 );
-}
+   }
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,6 +3,9 @@ import SearchBar from "./SearchBar";
 import styled from "styled-components"
 import { Link } from "react-router-dom";
 import { useLocation } from "react-router-dom";
+import { useSelector } from "react-redux"
+import { isElementType } from "@testing-library/user-event/dist/utils";
+import { useState } from "react";
 
 const DivNav = styled.nav`
 display: flex;
@@ -21,12 +24,34 @@ font-size: 30px
 
 function Nav(props) {
 
+
+ const estado = useSelector(state => state.myFavorites)  
   const Location = useLocation()
+const [fav, setFav] = useState(true)
+
+React.useEffect((fav) => {
+  if(estado){
+      setFav(true)
+    }
+  if (!estado){
+    setFav(false)
+    }   
+  }, [estado.name]);
+
+
+
 return (
     <DivNav>
        <SearchBar onSearch={props.onSearch}/>
 <Link to="/about"><li>About</li></Link>
 <Link to="/home"><li>Home</li></Link>
+{fav ? (
+      <><button>❤️</button><select>
+        {estado?.map((element) => <option><lu>{element.name}</lu></option>)}
+      </select></>	) : (
+					<button>🤍</button>
+				)}
+
     </DivNav>
 )}
 
