de1b9c51411fc01f28cbcc584b34ea564c6050cc
Santy1422 <102694038+Santy1422@users.noreply.github.com>
2022-11-13 20:40:04 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
+import App from "./App";
+import store from "./redux/store"
+import {Provider} from "react-redux"
+ReactDOM.render(
+	   <Provider store={store}>
+		  <BrowserRouter>
+			 <App />
+		  </BrowserRouter>
+	   </Provider>,
+	document.getElementById('root'),
+ );
